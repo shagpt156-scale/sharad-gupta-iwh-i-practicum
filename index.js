bacd6599ad1313b9b88ad6f5c384a8f3c868a7c1
Sharadgupta078 <guptasharad078@gmail.com>
2026-09-09 14:57:57 +0530

    completed the Integrating With HubSpot I Practicum task

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,21 +10,52 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_TOKEN;
+const CUSTOM_OBJECT_TYPE = '2-68964397';
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+app.get('/', async (req, res) => {
+    const objectsUrl = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE}?properties=name,bio,category`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
 
-// * Code for Route 1 goes here
+    try {
+        const resp = await axios.get(objectsUrl, { headers });
+        const data = resp.data.results;
+        res.render('homepage', { title: 'Custom Object List | Integrating With HubSpot I Practicum', data });
+    } catch (error) {
+        console.error(error);
+    }
+});
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+});
 
-// * Code for Route 2 goes here
+app.post('/update-cobj', async (req, res) => {
+    const objectsUrl = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE}`;
+    const newRecord = {
+        properties: {
+            name: req.body.name,
+            bio: req.body.bio,
+            category: req.body.category
+        }
+    };
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+    try {
+        await axios.post(objectsUrl, newRecord, { headers });
+        res.redirect('/');
+    } catch (error) {
+        console.error(error);
+    }
+});
 
-// * Code for Route 3 goes here
-
-/** 
+/**
 * * This is sample code to give you a reference for how you should structure your calls. 
 
 * * App.get sample
